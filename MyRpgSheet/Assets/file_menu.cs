d8e7d552a6f1076a449001357326834622047830
SilentShad <julien.boulicaut@epita.fr>
2019-03-22 17:12:45 +0100

    un commit

---
+++ MyRpgSheet/Assets/file_menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class file_menu : MonoBehaviour
+{
+  
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        
+    }
+}
